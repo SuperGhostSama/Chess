a0d8f2786403276d70c843770d923071470c51c7
“SuperGhost” <salaheddinehanaoui@gmail.com>
2023-09-24 01:42:39 +0100

    changed the code structure

--- Chess/src/PieceType.java
+++
@@ -1,8 +0,0 @@
-public enum PieceType {
-    PAWN,
-    ROOK,
-    KNIGHT,
-    BISHOP,
-    QUEEN,
-    KING
-}
---
+++ src/chess/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Chess Game Menu:");
+            System.out.println("1. Start New Game");
+            System.out.println("2. View Saved Games");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    // Start a new game
+                    startNewGame();
+                    break;
+                case 2:
+                    // View saved games (implement this functionality)
+                    viewSavedGames();
+                    break;
+                case 3:
+                    // Exit the game
+                    System.out.println("Exiting Chess Game. Goodbye!");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+                    break;
+            }
+        }
+    }
+
+    private static void startNewGame() {
+        // Implement the logic to start a new chess game
+        System.out.println("Starting a new game...");
+    }
+
+    private static void viewSavedGames() {
+        // Implement the logic to view saved games
+        System.out.println("Viewing saved games...");
+    }
+}
