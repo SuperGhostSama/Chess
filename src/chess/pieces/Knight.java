f8fb75e87a2c77bad1958851eb934dfde8efe765
Salah <salaheddinehanaoui@gmail.com>
2023-10-04 16:10:10 +0100

    made the knight capable of capturing opposing pieces

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -13,11 +13,18 @@ public class Knight {
 
         // Knights move in an L-shape: 2 squares in one direction and 1 square in the other
         if ((rowChange == 2 && colChange == 1) || (rowChange == 1 && colChange == 2)) {
-            // Check if the destination square is empty
-            return board[newRow][newCol].equals(" ");
+            // Check if the destination square is empty or contains a piece of the opposite color
+            String targetPiece = board[newRow][newCol];
+            if (targetPiece.equals(" ")) {
+                return true; // Move to an empty square
+            } else if (isWhite && Character.isUpperCase(targetPiece.charAt(0))) {
+                return true; // White knight can capture a black piece
+            } else if (!isWhite && Character.isLowerCase(targetPiece.charAt(0))) {
+                return true; // Black knight can capture a white piece
+            }
         }
 
         return false;
     }
-
 }
+
